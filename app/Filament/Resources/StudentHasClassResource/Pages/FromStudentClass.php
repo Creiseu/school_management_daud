749cd632a0e529e97fbfaaa494b0d09f9c48c0ae
daud <daudanggoro@gmail.com>
2024-07-24 16:31:23 +0700

    Custom Pages - Tutorial 10

---
+++ app/Filament/Resources/StudentHasClassResource/Pages/FromStudentClass.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Filament\Resources\StudentHasClassResource\Pages;
+
+use App\Filament\Resources\StudentHasClassResource;
+use App\Models\HomeRoom;
+use App\Models\Periode;
+use App\Models\Student;
+use App\Models\StudentHasClass;
+use Filament\Resources\Pages\Page;
+use Filament\Forms\Components\Card;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Concerns\InteractsWithForms;
+use Filament\Forms\Contracts\HasForms;
+
+class FromStudentClass extends Page implements HasForms
+{
+    use InteractsWithForms;
+    protected static string $resource = StudentHasClassResource::class;
+
+    protected static string $view = 'filament.resources.student-has-class-resource.pages.from-student-class';
+
+    public $students = [];
+    public $homerooms = '';
+    public $periode = '';
+
+    public function mount(){
+        $this->form->fill();
+    }
+
+    public function getFormSchema(): array
+    {
+        return [
+            Card::make()
+                ->schema([
+                    Select::make('students')
+                        ->multiple()
+                        ->label('Name Student')
+                        ->options(Student::all()->pluck('name', 'id'))
+                        ->columnSpan(3),
+                    Select::make('homerooms')
+                        ->options(HomeRoom::all()->pluck('classroom.name', 'id'))
+                        ->label('Class'),
+                    Select::make('periode')
+                        ->label('Periode')
+                        ->searchable()
+                        ->options(Periode::all()->pluck('name', 'id')),
+                ])->columns(3)
+        ];
+    }
+
+    public function save(){
+        $students = $this->students;
+        $insert = [];
+        foreach($students as $row){
+            array_push($insert, [
+                'students_id' => $row,
+                'homerooms_id' => $this->homerooms,
+                'periode_id' => $this->periode,
+                'is_open' => 1
+            ]);
+        }
+        StudentHasClass::insert($insert);
+
+        return redirect()->to('admin/student-has-classes');
+    }
+}
